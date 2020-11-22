61996567d27fc7bc4a36d8a30327bce7f4ba3103
Eva Janowski <eva716@live.unc.edu>
2020-11-22 18:10:35 -0500

    testing

---
+++ evatest/mainApp.js
@@ -0,0 +1,33 @@
+export const loadPage = async function() {
+    const $root = $('#root');
+    // let signup = await axios({
+    //     method:'post',
+    //     url: `https://stark-depths-67325.herokuapp.com/signup`,
+    //     withCredentials: true,
+    //     data: {
+    //         user: "Hannah",
+    //         password: "passwor",
+    //         email: "blah@gmail.com",
+    //         grad: "2021",
+    //         house: "Off campus",
+    //         pronouns: "she/her"
+    //     }
+    // })
+
+    /*let signin = await axios({
+        method:'post',
+        url: `https://stark-depths-67325.herokuapp.com/login`,
+        withCredentials: true,
+        data: {
+            user: "rachel",
+            password: "password",
+        }
+    })*/
+
+
+    $root.append(postingView());
+}
+
+$(function() {
+    loadPage();
+});
